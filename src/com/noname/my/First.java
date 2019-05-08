fdee56690daac03111be273647210e7342bb509b
Yuliya <k.a.yulia@gmail.com>
2019-05-08 11:10:32 +0200

    first commit

---
+++ src/com/noname/my/First.java
@@ -0,0 +1,18 @@
+package com.noname.my;
+
+import java.util.*;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+        System.getProperties().list(System.out);
+        System.out.println(System.getProperty("user.name"));
+        System.out.println(
+                System.getProperty("java.library.path")
+        );
+
+        System.out.println(
+                System.console()
+        );
+    }
+}
